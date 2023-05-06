3dbd6867b487afa753eb9fa0dee48b99dc01318c
sap-aar11 <sapana.gupta112011@gmail.com>
2023-05-07 00:47:10 +0530

    fourth commit

---
+++ Assignment_01/src/Pattern5.java
@@ -0,0 +1,21 @@
+
+public class Pattern5 {
+public static void kpattern()
+{
+
+	int n=14;
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0 ; j<n; j++)
+		{
+			//if(((i-j)>=(n-1)/2) || ((j+i)>=(n-1)+(n-1)/2))
+			if(((i+j<=(n-1)/2))||((i-j)>=(n-1)/2)||(i==0)||(i==n-1))
+				System.out.print("*");
+			else 
+				System.out.print(" ");
+		}
+		System.out.println();
+	}
+
+}
+}
