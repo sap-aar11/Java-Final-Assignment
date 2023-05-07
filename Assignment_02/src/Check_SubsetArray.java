a2b70608e9c4f37cdd749fb05c2cfd70e8793d32
sap-aar11 <sapana.gupta112011@gmail.com>
2023-05-07 23:17:16 +0530

    second commit

--- Assignment_02/src/Check_SubsetArray.java
+++ Assignment_02/src/Check_SubsetArray.java
@@ -2,18 +2,34 @@
 public class Check_SubsetArray {
 
 	public static void main(String[] args) {
-		int marr[] = {1,2,3,9,8,7};
+		int marr[] = {1,2,3,9,2,3,9};
 		int sarr[] = {2,3,9};
 		for(int i = 0; i < marr.length; i++)
 		{
 			boolean flag =false;
+			int k=0;
 			if(marr[i]==sarr[0])
 			{
-				for(int j=1;j<sarr.length;j++)
-					if
+				for(int j=0;j<sarr.length;j++)
+					{if(marr[i+j]==sarr[j])
+						k++;
+					}
+				
+				if(k==sarr.length)
+					{flag=true;
+					System.out.println("Array is subset of another Array");
+					break;
+					}
 			}
 		}
 
 	}
+	// printing array 
+    public static void printArray(int arr[]){
+        int len = arr.length;
+        for (int i = 0; i < len; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
 
 }
